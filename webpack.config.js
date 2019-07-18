4a4da0ee4ac05b580ca846ecab14d110043a5d8f
Abhinav <abhinav@devarkm.com>
2019-07-18 20:24:48 +0530

    added bable and webpack scripts with package json

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
+                loader: "file-loader?name=/static/[name].[ext]"
+            }
+        ],
+
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+};
